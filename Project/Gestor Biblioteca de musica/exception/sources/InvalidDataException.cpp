bd457cbd5db4d8dcc2ab6d26e6f96472db122065
godea-alexei <1241115@isep.ipp.pt>
2026-05-17 21:22:47 +0100

    Add files via upload

---
+++ Project/Gestor Biblioteca de musica/exception/sources/InvalidDataException.cpp
@@ -0,0 +1,10 @@
+
+
+InvalidDataException::InvalidDataException(string data){
+    this->data = data;
+}
+
+const char* InvalidDataException::what(){
+    string msg = "Error: ["+ data + "] is not valid!!";
+    return msg.c_str();
+}
