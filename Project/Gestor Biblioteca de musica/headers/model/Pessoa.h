059c9f94a9cf8a23bf4d85b90a1ef2ea9899f41b
godea <1241115@isep.ipp.pt>
2026-05-24 10:36:51 +0100

    implemented Musica.cpp, MusicaView.cpp, Pessoa.cpp, Musica.h, MusicaView.h & Pessoa.h

--- Project/Gestor Biblioteca de musica/headers/model/Pessoa.h
+++ Project/Gestor Biblioteca de musica/headers/model/Pessoa.h
@@ -4,5 +4,19 @@
 
 #ifndef PESSOA_H
 #define PESSOA_H
-
 #endif //PESSOA_H
+#include <string>
+
+class Pessoa{
+
+    public:
+        std::string nome;
+        int anoNascimento;
+
+    static std::string getNome();
+    static std::string getIdade();
+
+
+
+
+}
--- Project/Gestor Biblioteca de musica/headers/views/MusicaView.h
+++ Project/Gestor Biblioteca de musica/headers/views/MusicaView.h
@@ -1,8 +1,21 @@
-//
-// Created by faixe on 19/05/2026.
-//
 
 #ifndef VIEWMUSICA_H
 #define VIEWMUSICA_H
 
 #endif //VIEWMUSICA_H
+
+
+#pragma once
+#include <string>
+#include <list>
+#include "Musica.h"
+#include "Util.h"
+
+class ViewMusicas {
+
+    public:
+        Musica obterMusica(std::string nome);
+        std::list<Musica> listarMusicas();
+        std::string detalhesMusica(std::string nome);
+
+};
