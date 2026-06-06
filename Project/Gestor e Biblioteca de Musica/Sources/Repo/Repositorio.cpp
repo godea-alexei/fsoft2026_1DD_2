abfabb3b526b8885e83c94e74a5a7a9d4e8c34d8
godea-alexei <1241115@isep.ipp.pt>
2026-06-06 08:33:43 +0100

    created repo

---
+++ Project/Gestor e Biblioteca de Musica/Sources/Repo/Repositorio.cpp
@@ -0,0 +1,7 @@
+#include "Repositorio.h"
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <string>
+using namespace std;
+
---
+++ Project/Gestor e Biblioteca de Musica/Sources/Repo/Repositorio.h
@@ -0,0 +1,37 @@
+#ifndef GESTOR_E_BIBLIOTECA_DE_MUSICA_REPOSITORIO_H
+#define GESTOR_E_BIBLIOTECA_DE_MUSICA_REPOSITORIO_H
+
+
+class Repositorio {
+
+
+    public:
+        void carregarMusicas();
+        void carregarEditoras();
+        void carregarArtistas();
+        void carregarAlbums();
+        void carregarUtilizadores();
+        void carregarListas();
+        void carregarAlbuns();
+
+        void guardarMusicas();
+        void guardarEditoras();
+        void guardarArtistas();
+        void guardarAlbums();
+        void guardarUtilizadores();
+        void guardarListas();
+        void guardarAlbuns();
+
+        void eliminarMusicas();
+        void eliminarEditoras();
+        void eliminarArtistas();
+        void eliminarAlbums();
+        void eliminarUtilizadores();
+        void eliminarListas();
+        void eliminarAlbuns();
+
+
+};
+
+
+#endif //GESTOR_E_BIBLIOTECA_DE_MUSICA_REPOSITORIO_H
