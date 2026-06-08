e1a1e178ba3f3ea9e7007bf5a3a2ccf88b4f6555
jhmjacome1201253 <1201253@isep.ipp.pt>
2026-06-08 15:54:35 +0100

    Update AlbumContainer.h

--- Project/Gestor Biblioteca de musica/headers/model/AlbumContainer.h
+++ Project/Gestor Biblioteca de musica/headers/model/AlbumContainer.h
@@ -1,8 +1,24 @@
-//
-// Created by faixe on 19/05/2026.
-//
-
 #ifndef ALBUMCONTAINER_H
 #define ALBUMCONTAINER_H
 
-#endif //ALBUMCONTAINER_H
+#include <string>
+#include <list>
+#include "Album.h"
+using namespace std;
+
+class AlbumContainer {
+
+private:
+    list<Album*> albuns;
+
+public:
+    AlbumContainer();
+    ~AlbumContainer();
+
+    void adicionarAlbum();
+    void removerAlbum();
+    Album* procurarAlbum(string nome);
+    list<Album*>& getAll();
+};
+
+#endif
